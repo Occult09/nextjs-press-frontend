0d873475a1a524f81943b0433f3830346a35fafc
Maheer Musfiq <maheermusfiq221@gmail.com>
2026-09-21 15:51:19 +0600

    completed project route structure

---
+++ app/(dashboardGroup)/layout.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const DashboardLayout = (
+    {
+        children
+    }: {
+        children: React.ReactNode
+    }
+) => {
+    return (
+        <div>
+            {children}
+        </div>
+    )
+}
+
+export default DashboardLayout
