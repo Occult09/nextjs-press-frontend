1fccc1e9cc0a4f9e449b8cd766353e00b46e61a5
Maheer Musfiq <maheermusfiq221@gmail.com>
2026-09-24 19:40:20 +0600

    implemented server action for login form and register form

---
+++ app/(authGroup)/_actions/authActions.ts
@@ -0,0 +1,50 @@
+"use server";
+
+export const loginAction = async (formData: FormData) => {
+
+    const email = formData.get("email");
+    const password = formData.get("password");
+
+    const payload = {
+        email,
+        password
+    }
+
+    const res = await fetch(`${process.env.BACKEND_API_URL}/api/auth/login`, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(payload)
+    })
+
+    const result = await res.json();
+
+    console.log(result)
+}
+
+export const registerAction = async(formData: FormData) => {
+    const name = formData.get("name")
+    const email = formData.get("email");
+    const password = formData.get("password");
+    const profilePhoto = formData.get("profilePhoto")
+
+    const payload = {
+        name,
+        email,
+        password,
+        profilePhoto
+    }
+
+    const res = await fetch(`${process.env.BACKEND_API_URL}/api/user/register`, {
+        method: "POST",
+        headers: {
+            "Content-Type" : "application/json"
+        },
+        body: JSON.stringify(payload)
+    })
+
+    const result = await res.json();
+
+    console.log(result);
+}
--- app/(authGroup)/_components/LoginForm.tsx
+++ app/(authGroup)/_components/LoginForm.tsx
@@ -4,17 +4,18 @@ import { Button } from '@/components/ui/button'
 import { Card } from '@/components/ui/card'
 import { Input } from '@/components/ui/input'
 import React from 'react'
+import { loginAction } from '../_actions/authActions'
 
 const LoginForm = () => {
   return (
-    <form className='p-2 space-y-3'>
-        <Card className='p-6 space-y-5 shadow-sm'>
-            <Input className='h-11' name='email' type='email' placeholder='Enter Your Email' required/>
-            <Input className='h-11' name='password' type='password' placeholder='Enter Your Password' required/>
-            <Button className='w-full h-11 font-semibold' type='submit'>
-                Login
-            </Button>
-        </Card>
+    <form action={loginAction} className='p-2 space-y-3'>
+      <Card className='p-6 space-y-5 shadow-sm'>
+        <Input className='h-11' name='email' type='email' placeholder='Enter Your Email' required />
+        <Input className='h-11' name='password' type='password' placeholder='Enter Your Password' required />
+        <Button className='w-full h-11 font-semibold' type='submit'>
+          Login
+        </Button>
+      </Card>
     </form>
   )
 }
